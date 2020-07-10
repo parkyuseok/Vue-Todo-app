11eb09208609e53abeb3feb27b64f7530cdb97b0
yuseok <znql16@gmail.com>
2020-07-10 14:04:33 +0900

    CSS전처리(Preproces_SCSS)와 후처리(PostCSS_Autoprefixer) 모듈 설정 도중 ValidationError 발생...

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,94 @@
-const path = require('path')
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path') // 파일이나 디렉터리 경로를 다루기 위한 NodeJS 기본 모듈
+const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const merge = require('webpack-merge')
 require('@babel/polyfill')
 
 module.exports = (env, opts) => {
     const config = {
-        // 중복되는 옵션들...
-        // 진입점
+        // 파일을 읽어들이기 시작하는 진입
+        // `__dirname`은 현재 파일의 위치를 알려주는 NodeJS 전역 변수
         entry: {
             app: [
                 '@babel/polyfill',
                 path.join(__dirname, 'main.js')
             ]
         },
-        // 결과물에 대한 설정
+        // 결과물(번들)을 반환하는 설정
+        // `[name]`은 `entry`의 Key 이름, `app`
         output: {
             filename: '[name].js', //app.js
             path: path.join(__dirname, 'dist')
         },
+        // 모듈 처리 방식을 설정
         module: {
             rules: [
-                // ... other rules
                 {
-                test: /\.vue$/,
-                use: 'vue-loader'
+                    test: /\.vue$/,
+                    use: 'vue-loader'
                 },
-                // this will apply to both plain `.js` files
-                // AND `<script>` blocks in `.vue` files
                 {
                     test: /\.js$/,
                     exclude: /node_modules/,
                     use: 'babel-loader'
                 },
-                // this will apply to both plain `.css` files
-                // AND `<style>` blocks in `.vue` files
                 {
                     test: /\.css$/,
                     use: [
                     'vue-style-loader',
                     'css-loader'
                     ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'vue-style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
                 }
             ]
         },
+        // 번들링 후 결과물의 처리 방식 등 다양한 플러그인들을 설정
         plugins: [
-            // make sure to include the plugin!
-            new VueLoaderPlugin(),
             new HtmlWebpackPlugin({
                 template: path.join(__dirname, 'index.html')
             }),
-            new CopyPlugin({
-                patterns: [
-                    { 
-                        from: 'assets/',
-                        to: ''
-                    }
-                ]
-            })
+            new VueLoaderPlugin(),
+            // assets 디렉터리의 내용을 `dist` 디렉터리에 복사합니다.
+            new CopyPlugin([
+                { 
+                    from: 'assets/',
+                    to: ''
+                }
+            ])
         ]
     }
 
     // 개발용
-    if (this.opts.mode === 'development') {
+    if (opts.mode === 'development') {
         return merge(config, {
-            //추가 개발용 옵션
+            //추가 개발용 옵션, 빌드 시간이 적고, 디버깅이 가능한 방식
             devtool: 'eval',
             devServer: {
+                // 자동으로 기본 브라우저를 오픈합니다.
                 open: false,
+                // HMR, https://webpack.js.org/concepts/hot-module-replacement
                 hot: true
             }
         })
 
-    // 제품용
+    // 제품용, opts.mode === 'production'
     } else {
         return merge(config, {
-            //추가 제출용 옵션
+            //추가 제출용 옵션, 용량이 적은 방식
             devtool: 'cheap-module-source-map',
             plugins: [
+                // 빌드(build) 직전 `output.path`(`dist` 디렉터리) 내 기존 모든 파일 삭제
                 new CleanWebpackPlugin()
             ]
         })
     }
-
 }
