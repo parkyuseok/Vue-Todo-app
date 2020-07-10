a07a2afd7b8b6d5af7c3825a4c218b7c34fcd468
yuseok <znql16@gmail.com>
2020-07-10 17:40:27 +0900

    각 컴포넌트 생성 및 연결

--- App.vue
+++ App.vue
@@ -1,21 +1,22 @@
 <template>
-    <h1>{{ msg }}</h1>
+  <todo-app />
 </template>
 
 <script>
+import TodoApp from './components/TodoApp' //.vue 생략가능
+
 export default {
-  data () {
-    return {
-      msg: 'Hello Vue.'
-    }
+  components: {
+    /*  가지고 온 컴포넌트를 명시해준다.
+        TodoApp라는 이름을 components에 연결하기 위해서
+        'todo-app': TodoApp는 'todo-app'이라는 이름의 컴포넌트를 사용할 것이고 
+        그건 내가 가지고온 TodoApp이라는 컴포넌트이다.
+        + 컴포넌트를 사용할 때 vue 내부에서
+        TodoApp(PascalCase 표기법)은 kebab-case로 고쳐준다.
+        그래서 'TodoApp': TodoApp이라고 작성할 수 있고
+        속성명과 값이 같으면 생략해서 작성할 수 있다.
+        따라서 'todo-app': TodoApp => 'TodoApp': TodoApp => TodoApp */
+        TodoApp
   }
 }
 </script>
-
-<style scoped lang="scss">
-    $color: blue;
-    h1 {
-        color: color;
-        display: flex;
-    }
-</style>
---
+++ components/TodoApp.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- template 태그 내부에는 자식요소 1개만 들어갈 수 있어서 div를 만들어서 넣어준다. -->
+    <div>
+        <todo-item></todo-item>
+        <todo-creator></todo-creator>
+    </div>
+</template>
+
+<script>
+import TodoCreator from './TodoCreator' //TodoCreator라는 이름으로 가져온다.
+import TodoItem from './TodoItem'
+
+export default {
+    // 외부에서 가져온 컴포넌트를 연결하는 옵션
+    components: {
+        // 가져온 컴포넌트를 연결해준다.
+        TodoCreator,
+        TodoItem
+    }
+}
+</script>
