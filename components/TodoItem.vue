a1740b24fad9f1043b96df34ce23c79ba9c5523e
yuseok <znql16@gmail.com>
2020-07-13 14:24:02 +0900

    TodoItem 컴포넌트만들기 - 수정 모드

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,24 +1,54 @@
 <template>
     <div class="todo-item">
 
-        <!--todo item이 완료된 일인지 해야할 일인지 체크할 체크박스 
-            v-model을 통해서 done이라는 computed data 부분을 바인드 시켜주고
-            done이 true면 check가 되어있는 상태 false면 체크 X -->
-        <input 
-            v-model="todo.done"
-            type="checkbox"
-        />
-        <div class="item__title-wrap">
-            <div class="item__title">
-                {{ todo.title}}
-            </div>
-            <div class="item__date">
-                {{ date }}
+        <div 
+            v-if="isEditMode"
+            class="item__inner item--edit"
+        >
+            <input 
+                ref="titleInput"
+                :value="editedTitle"
+                type="text"
+                @input="editedTitle - $event.target.value"
+                @keypress.enter="editedTodo"
+                @keypress.esc="offEditMode"
+            >
+            <div class="item__action">
+                <button 
+                    key="complete"
+                    @click="editedTodo">완료</button>
+                <button 
+                    key="cancel"
+                    @click="offEditMode">취소</button>
             </div>
         </div>
-        <div class="item__actions">
-            <button @click="onEditMode">수정</button>
-            <button @click="deleteTodo">삭제</button>
+        <div 
+            v-else
+            class="item__inner item--normal"
+        >
+            <!--todo item이 완료된 일인지 해야할 일인지 체크할 체크박스 
+            v-model을 통해서 done이라는 computed data 부분을 바인드 시켜주고
+            done이 true면 check가 되어있는 상태 false면 체크 X -->
+            <input 
+                v-model="todo.done"
+                type="checkbox"
+            />
+            <div class="item__title-wrap">
+                <div class="item__title">
+                    {{ todo.title}}
+                </div>
+                <div class="item__date">
+                    {{ date }}
+                </div>
+            </div>
+            <div class="item__actions">
+                <button 
+                    key="update"
+                    @click="onEditMode">수정</button>
+                <button 
+                    key="delete"
+                    @click="deleteTodo">삭제</button>
+            </div>
         </div>
 
     </div>
@@ -31,6 +61,13 @@ export default {
     props: {
         // todo라는 이름으로 받아 쓸건데 type이 배열안에 있는 Object 형태로 각각의 todo 아이템들이 들어올 것이다.라고 명시해준 것
         todo: Object
+    },
+    data () {
+        return {
+            isEditMode: false,
+            //title을 직접 바꾸면 취소하고 싶어도 원래 값을 뭔지 모르므로 돌아갈 수 없다.
+            editedTitle: ''
+        }
     },
     computed: {
         // done이라는 computed data를 객체 형식으로 작성.
@@ -60,11 +97,28 @@ export default {
         }
     },
     methods: {
-        onEditMode () {
+        editedTodo () {
+            // 수정전 title !== 수정후 title이 같지 않아야만
+            if (this.todo.title !== this.editedTitle){
+                this.updateTodo({
+                    titld: this.editedTitle,
+                    updatedAt: new Date()
+                })
+            }
 
+            this.offEditMode()
+        },
+        onEditMode () {
+            this.isEditMode = true
+            this.editedTitle = this.todo.title
+            // 1. isEditMode를 true로 해서 화면을 바꾸어 줄건데 
+            // 그것이 갱신되고 나서 렌더링이 끝나면
+            this.$nextTick(() => { //$nextTick는 화면이 렌더링이 되었다는 것을 보장시킬 때 사용
+                this.$refs.titleInput.focus() //2.그 때 titleInput을 찾아서 포커싱 하겠다.
+            })
         },
         offEditMode () {
-            
+            this.isEditMode = false
         },
         updateTodo (value) {
             // ('event-Name', 
