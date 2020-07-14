77f0a24ddf722cd24b10e1010889cf5685a1a1ed
yuseok <znql16@gmail.com>
2020-07-14 15:46:04 +0900

    Vuex 설치

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router' // './router/index.js' index.js는 생략가능하다.
+import store from './store' // index.js는 생략 가능
 
 new Vue({
   el: '#app',
+  // Vue에 연결
   router,
+  store,
   render: h => h(App) // https://goodteacher.tistory.com/85
 })
 
