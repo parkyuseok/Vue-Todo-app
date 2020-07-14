460969cf6b3e5dee7d9c4251277dfb5ae3c466aa
yuseok <znql16@gmail.com>
2020-07-14 10:32:22 +0900

    Veu-Router 설치 및 기본 구성

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import router from './router' // './router/index.js' index.js는 생략가능하다.
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
