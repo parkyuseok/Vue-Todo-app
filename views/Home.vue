2561b84f5a2c95fa9c69be5606ce064b9c4df04f
yuseok <znql16@gmail.com>
2020-07-14 11:20:59 +0900

    Home 페이지 구성 - 클릭 이벤트를 이용해서 특정 페이지로 이동하는 코드

---
+++ views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="home">
+        <div>
+            <h1>
+                YUSEOK's Home page!
+            </h1>
+            <h2 @click="toAbout">
+                About
+            </h2>
+            <!-- script의 코드에 적는것이 번거롭다면 this를 빼고 작성하면 된다. -->
+            <h2 @click="$router.push('/todos')">
+                Todo App
+            </h2>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        toAbout () {
+            // 특정한 페이지로 밀어서 보내겠다.
+            this.$router.push('/about')
+        }
+    }
+}
+</script>
