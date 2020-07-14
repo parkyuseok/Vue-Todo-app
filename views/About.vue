c698452b1ef2c112c27418422ff939d4b49424c8
yuseok <znql16@gmail.com>
2020-07-14 11:30:24 +0900

    Home - router-link componente, About 페이지 구성

---
+++ views/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <section class="about">
+        <div>
+            <h2>Park Yu-Seok</h2>
+            <h2>znql16@gmail.com</h2>
+            <h2>010-5128-6967</h2>
+        </div>
+    </section>
+</template>
--- views/Home.vue
+++ views/Home.vue
@@ -4,24 +4,26 @@
             <h1>
                 YUSEOK's Home page!
             </h1>
-            <h2 @click="toAbout">
+            <!-- script의 코드에 적는것이 번거롭다면 this를 빼고 작성하면 된다. -->
+            <h2 @click="$router.push('/about')">
                 About
             </h2>
-            <!-- script의 코드에 적는것이 번거롭다면 this를 빼고 작성하면 된다. -->
-            <h2 @click="$router.push('/todos')">
-                Todo App
-            </h2>
+            <router-link 
+                to="/todos" 
+                tag="h2">
+                    Todo App
+            </router-link>
         </div>
     </section>
 </template>
 
-<script>
-export default {
-    methods: {
-        toAbout () {
-            // 특정한 페이지로 밀어서 보내겠다.
-            this.$router.push('/about')
-        }
-    }
-}
-</script>
+// <script>
+// export default {
+//     methods: {
+//         toAbout () {
+//             // 특정한 페이지로 밀어서 보내겠다.
+//             this.$router.push('/about')
+//         }
+//     }
+// }
+// </script>
