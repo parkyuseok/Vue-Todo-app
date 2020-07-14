d788d2c07cd7f1797c2f91f2cfe047748fa1abdf
yuseok <znql16@gmail.com>
2020-07-14 11:59:45 +0900

    Router 관련 주석 추가함

--- views/Home.vue
+++ views/Home.vue
@@ -8,6 +8,9 @@
             <h2 @click="$router.push('/about')">
                 About
             </h2>
+            <!-- 네비게이션을 위해 router-link 컴포넌트를 사용합니다. -->
+            <!-- 구체적인 속성은 `to` prop을 이용합니다. -->
+            <!-- 기본적으로 `<router-link>`는 `<a>` 태그로 렌더링됩니다.-->
             <router-link 
                 to="/todos" 
                 tag="h2">
