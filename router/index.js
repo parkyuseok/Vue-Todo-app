1d284d27515d48c4c456e98a4c7c7fc8dbeba578
yuseok <znql16@gmail.com>
2020-07-16 16:35:04 +0900

    router/index.js 주석 추가

--- router/index.js
+++ router/index.js
@@ -10,14 +10,17 @@ Vue.use(VueRouter)
 const routes = [
   // config(구성)
   {
+    name: 'index',
     path: '/', // /가 하는 역할은 "이 페이지의 루트 페이지이다(최상위 페이지)"
     component: Home
   },
   {
+    name: 'about',
     path: '/about',
     component: About
   },
   {
+    name: 'todos',
     path: '/todos',
     redirect: '/todos/all',
     component: TodoApp,
@@ -31,5 +34,7 @@ const routes = [
 ]
 
 export default new VueRouter({
+  // https://router.vuejs.org/kr/guide/essentials/history-mode.html
+  // mode: 'history',
   routes // `routes: routes`의 줄임
 })
