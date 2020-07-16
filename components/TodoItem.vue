95e6571705398f57c5b66f72ad0f6524bfd625b5
yuseok <znql16@gmail.com>
2020-07-16 16:40:40 +0900

    components/TodoItem.vue 코드 리팩토링, 수정버튼 기능 Fix bug

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -12,7 +12,7 @@
                 ref="titleInput"
                 :value="editedTitle"
                 type="text"
-                @input="editedTitle - $event.target.value"
+                @input="editedTitle = $event.target.value"
                 @keydown.enter="editedTodo"
                 @keydown.esc="offEditMode"
             >
@@ -79,18 +79,16 @@
 </template>
 
 <script>
-import dayjs from 'dayjs' // moment.js 경량화 버전(다른 라이브러리 이지만 같은 문법)
+import dayjs from 'dayjs'
 
 export default {
   name: 'TodoItem',
   props: {
-    // todo라는 이름으로 받아 쓸건데 type이 배열안에 있는 Object 형태로 각각의 todo 아이템들이 들어올 것이다.라고 명시해준 것
     todo: Object
   },
   data () {
     return {
       isEditMode: false,
-      // title을 직접 바꾸면 취소하고 싶어도 원래 값을 뭔지 모르므로 돌아갈 수 없다.
       editedTitle: this.todo.title
     }
   },
@@ -99,8 +97,6 @@ export default {
       get () {
         return this.todo.done
       },
-      // done이라는 computed data가 변경되면 set()이라는 메소드가 반응하는데
-      // Setter는 변경된 값을 매개변수로 받아서 쓸 수 있다.
       set (done) {
         this.updateTodo({
           done
@@ -122,8 +118,8 @@ export default {
       this.editedTitle = this.todo.title
       this.isEditMode = true
       // Vue.js가 데이터 변경 후 DOM 업데이트를 마칠 때까지 기다림.
-      this.$nextTick(() => { // $nextTick는 화면이 렌더링이 되었다는 것을 보장시킬 때 사용
-        this.$refs.titleInput.focus() // 2.그 때 titleInput을 찾아서 포커싱 하겠다.
+      this.$nextTick(() => {
+        this.$refs.titleInput.focus()
       })
     },
     offEditMode () {
@@ -141,17 +137,12 @@ export default {
       this.offEditMode()
     },
     updateTodo (value) {
-      // ('event-Name',
-      // 어떤 todo가 업데이트 되는지 알아야 되기 때문에 현재 객체 this.todo를 적어주고,
-      // 어떠한 값이 업데이트 될 것인지)
-      // this.$emit('update-todo', this.todo, value)
       this.$store.dispatch('todoApp/updateTodo', {
         todo: this.todo,
         value
       })
     },
     deleteTodo () {
-      // this.$emit('delete-todo', this.todo)
       this.$store.dispatch('todoApp/deleteTodo', this.todo)
     }
   }
