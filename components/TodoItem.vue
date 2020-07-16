014252069ed49a81bd0c548316dec8a030f80a93
yuseok <znql16@gmail.com>
2020-07-16 16:05:23 +0900

    components/TodoItem.vue 코드 리팩토링 작업중

--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -3,7 +3,7 @@
         :class="{ done: done }"
         class="todo-item"
     >
-
+        <!-- EDIT -->
         <div
             v-if="isEditMode"
             class="item__inner item--edit"
@@ -13,31 +13,32 @@
                 :value="editedTitle"
                 type="text"
                 @input="editedTitle - $event.target.value"
-                @keypress.enter="editedTodo"
-                @keypress.esc="offEditMode"
+                @keydown.enter="editedTodo"
+                @keydown.esc="offEditMode"
             >
             <div class="item__action">
                 <button
                     class="btn btn--primary"
                     key="complete"
-                    @click="editedTodo">
-                    <i class="material-icons">done</i>
-                    </button>
+                    @click="editedTodo"
+                >
+                  <i class="material-icons">done</i>
+                </button>
                 <button
                     class="btn"
                     key="cancel"
-                    @click="offEditMode">
-                    <i class="material-icons">clear</i>
-                    </button>
+                    @click="offEditMode"
+                >
+                  <i class="material-icons">clear</i>
+                </button>
             </div>
         </div>
+
+        <!-- NORMAL -->
         <div
             v-else
             class="item__inner item--normal"
         >
-            <!--todo item이 완료된 일인지 해야할 일인지 체크할 체크박스
-            v-model을 통해서 done이라는 computed data 부분을 바인드 시켜주고
-            done이 true면 check가 되어있는 상태 false면 체크 X -->
             <label>
                 <input
                     v-model="done"
@@ -48,7 +49,9 @@
                 </span>
             </label>
             <div class="item__title-wrap">
-                <div class="item__title">
+                <div 
+                  class="item__title"
+                  @dblclick="onEditMode">
                     {{ todo.title}}
                 </div>
                 <div class="item__date">
@@ -59,18 +62,19 @@
                 <button
                     class="btn"
                     key="update"
-                    @click="onEditMode">
-                    <i class="material-icons">edit</i>
-                    </button>
+                    @click="onEditMode"
+                >
+                  <i class="material-icons">edit</i>
+                </button>
                 <button
                     class="btn btn--danger"
                     key="delete"
-                    @click="deleteTodo">
-                    <i class="material-icons">delete</i>
-                    </button>
+                    @click="deleteTodo"
+                >
+                  <i class="material-icons">delete</i>
+                </button>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -78,6 +82,7 @@
 import dayjs from 'dayjs' // moment.js 경량화 버전(다른 라이브러리 이지만 같은 문법)
 
 export default {
+  name: 'TodoItem',
   props: {
     // todo라는 이름으로 받아 쓸건데 type이 배열안에 있는 Object 형태로 각각의 todo 아이템들이 들어올 것이다.라고 명시해준 것
     todo: Object
@@ -86,11 +91,10 @@ export default {
     return {
       isEditMode: false,
       // title을 직접 바꾸면 취소하고 싶어도 원래 값을 뭔지 모르므로 돌아갈 수 없다.
-      editedTitle: ''
+      editedTitle: this.todo.title
     }
   },
   computed: {
-    // done이라는 computed data를 객체 형식으로 작성.
     done: {
       get () {
         return this.todo.done
@@ -98,15 +102,12 @@ export default {
       // done이라는 computed data가 변경되면 set()이라는 메소드가 반응하는데
       // Setter는 변경된 값을 매개변수로 받아서 쓸 수 있다.
       set (done) {
-        // computed data에 직접적으로 값을 할당할 때
-        // 할당받은 값을 어떻게 처리할 것인가 이 부분에 로직으로 작성할 수 있다.
         this.updateTodo({
-          done // done: done, done에다가 전달받은 done을 update 시킨다.
+          done
         })
       }
     },
     date () {
-      // https://momentjs.com/docs/#/displaying/
       const date = dayjs(this.todo.createdAt)
       const isSame = date.isSame(this.todo.updatedAt)
       if (isSame) {
@@ -117,28 +118,27 @@ export default {
     }
   },
   methods: {
-    editedTodo () {
-      // 수정전 title !== 수정후 title이 같지 않아야만
-      if (this.todo.title !== this.editedTitle) {
-        this.updateTodo({
-          title: this.editedTitle,
-          updatedAt: new Date()
-        })
-      }
-
-      this.offEditMode()
-    },
     onEditMode () {
-      this.isEditMode = true
       this.editedTitle = this.todo.title
-      // 1. isEditMode를 true로 해서 화면을 바꾸어 줄건데
-      // 그것이 갱신되고 나서 렌더링이 끝나면
+      this.isEditMode = true
+      // Vue.js가 데이터 변경 후 DOM 업데이트를 마칠 때까지 기다림.
       this.$nextTick(() => { // $nextTick는 화면이 렌더링이 되었다는 것을 보장시킬 때 사용
         this.$refs.titleInput.focus() // 2.그 때 titleInput을 찾아서 포커싱 하겠다.
       })
     },
     offEditMode () {
       this.isEditMode = false
+    },
+    editedTodo () {
+      // 수정전 title !== 수정후, 수정한 내용이 있는 경우만 저장!
+      if (this.todo.title !== this.editedTitle) {
+        this.updateTodo({
+          title: this.editedTitle,
+          updatedAt: new Date()
+        })
+      }
+      // 수정 모드 종료.
+      this.offEditMode()
     },
     updateTodo (value) {
       // ('event-Name',
