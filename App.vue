437203ca10d07f0c39ccb3b4f407ae1d33d9bb65
yuseok <znql16@gmail.com>
2020-07-16 16:04:45 +0900

    App.vue 코드 리팩토링 작업중

--- App.vue
+++ App.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
+    <!-- HOME BUTTON -->
     <router-link
       to="/"
-      class="to-home">
+      class="to-home"
+    >
       <i class="material-icons">home</i>
     </router-link>
 
-    <!-- 라우트 아울렛 -->
-    <!-- 현재 라우트에 맞는 컴포넌트가 렌더링됩니다. -->
+    <!-- VIEWS -->
     <router-view />
   </div>
 </template>
